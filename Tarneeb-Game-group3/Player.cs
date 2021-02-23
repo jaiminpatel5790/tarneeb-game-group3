401f91b79ce21aa9badd37df4ddde98e61c2078b
Rut Patel <patelrut238@gmail.com>
2021-02-23 14:26:06 -0500

    Updated Cards Class & Added Players class

---
+++ Tarneeb-Game-group3/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cards;
+
+namespace Tarneeb_Game_group3
+{
+    class Player
+    {
+        public string name { get; set; }
+        public List<Card> playersCards = new List<Card>();
+
+
+
+
+
+        public Player(string name, List<Card> playersCards)
+        {
+
+
+            this.name = name;
+            this.playersCards = playersCards;
+        }
+
+    }
+}
--- Tarneeb-Game-group3/Program.cs
+++ Tarneeb-Game-group3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Cards;
+using System.Collections.Generic;
 
 namespace Tarneeb_Game_group3
 {
@@ -6,9 +8,31 @@ namespace Tarneeb_Game_group3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("123");
-            Console.WriteLine("He is busy ");   
-            Console.WriteLine("Kaif is busy kaif ");
+            var deck = new Deck();
+
+            deck.Shuffle();
+            List<Card> pass1 = deck.Sort(deck.TakeCards(13));
+            List<Card> pass2 = deck.TakeCards(13);
+            List<Card> pass3 = deck.TakeCards(13);
+            List<Card> pass4 = deck.TakeCards(13);
+
+
+            Console.WriteLine("Player 1 Cards:");
+            Player player1 = new Player("Alaadin Addas", pass1);
+            player1.playersCards.ForEach(Console.WriteLine);
+            Console.WriteLine("_____________________________________");
+            Console.WriteLine("Player 2 Cards:");
+            Player player2 = new Player("John Doe", pass2);
+            player2.playersCards.ForEach(Console.WriteLine);
+            Console.WriteLine("_____________________________________");
+            Console.WriteLine("Player 3 Cards:");
+            Player player3 = new Player("Jane Doe", pass3);
+            player3.playersCards.ForEach(Console.WriteLine);
+            Console.WriteLine("_____________________________________");
+            Console.WriteLine("Player 4 Cards:");
+            Player player4 = new Player("Some Doe", pass4);
+            player4.playersCards.ForEach(Console.WriteLine);
+
         }
     }
 }
