b3c7dbb5fb2c0e6531e004d975c3e80ed3a09e73
Kaif <kaifvakil02@gmail.com>
2021-02-25 13:00:23 -0500

    Bid class logic

--- Tarneeb-Game-group3/Program.cs
+++ Tarneeb-Game-group3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Cards;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Tarneeb_Game_group3
 {
@@ -16,6 +18,7 @@ namespace Tarneeb_Game_group3
             List<Card> pass3 = deck.Sort(deck.TakeCards(13));
             List<Card> pass4 = deck.Sort(deck.TakeCards(13));
 
+            
 
             Console.WriteLine("Player 1 Cards:");
             Player player1 = new Player("Alaadin Addas", pass1);
@@ -36,24 +39,48 @@ namespace Tarneeb_Game_group3
             Team FirstTeam = new Team(player1, player2);
             Team SecondTeam = new Team(player3, player4);
 
+            List<Player> playerList = new List<Player>();
+            playerList.Add(player1);
+            playerList.Add(player2);
+            playerList.Add(player3);
+            playerList.Add(player4);
+
+            
+
             Random number = new Random();
-            int BidPlayer = number.Next(1,4);
+           
+            int BidPlayer = number.Next(1,5);
+           
+
             Console.WriteLine(BidPlayer);
             int counter = 0;
+
             for(int i = BidPlayer; i <= 8; i++)
             {
+                int secondBid = number.Next(1, 5);
+                //Player randomPlayer = playerList[BidPlayer];
+                Console.WriteLine("Player " + playerList[secondBid]);
+                
                 Console.WriteLine(i);
+                //if (i == 1)
+                //{
+                //    int num = 1;
+                //    Object obj = num;
+                //}
                 if(BidPlayer < 4)
                 {
                     //Bid.CreateBid(player[BidPlayer], tricks, suit);
                 }
 
                 counter++;
+                Console.WriteLine("Counter " + counter);
+                
                 if(counter == 4)
                 {
                     break;
                 }
             }
+           
 
 
 
